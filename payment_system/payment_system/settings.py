97d144a0af998305f73d212588f53b3d19f9266a
krishnarajav <118679169+krishnarajav@users.noreply.github.com>
2024-07-04 21:56:50 +0530

    configured files

--- payment_system/payment_system/settings.py
+++ payment_system/payment_system/settings.py
@@ -51,9 +51,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    
+    'login.middlewares.LoginRequiredMiddleware',
 ]
-"""'login.middlewares.LoginRequiredMiddleware',"""
 
 LOGIN_URL = '/login/'
 
