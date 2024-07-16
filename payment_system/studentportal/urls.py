1da574cc1b1a159348df5e87f3ea3489ae2f8649
krishnarajav <118679169+krishnarajav@users.noreply.github.com>
2024-07-16 16:11:41 +0530

    minor changes

--- payment_system/studentportal/urls.py
+++ payment_system/studentportal/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('dashboard/', views.dashboard, name='dashboard'),
+    path('dashboard/', views.dashboard, name='student_dashboard'),
     path('payment/', views.payment, name='student_payment'),  
     path('queries/', views.queries, name='student_queries'),  
     path('settings/', views.acc_settings, name='student_settings'),  
