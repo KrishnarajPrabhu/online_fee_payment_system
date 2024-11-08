cdf98269f3891ba3fb6ecbbbd192a6ddcbd161a6
Krishnaraja V <krishnarajav03@gmail.com>
2024-11-08 13:59:33 +0530

    updating settings.py

--- payment_system/payment_system/settings.py
+++ payment_system/payment_system/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-n#a8dy_cg$a-#)8g6(xj=i$3ic^rbv%g#-vx+exz3cgza+48tx'
+SECRET_KEY =
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -151,6 +151,6 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'maheshnayak5649@gmail.com'
-EMAIL_HOST_PASSWORD = 'zkvo ljms zoso pttp'
+EMAIL_HOST_USER = 
+EMAIL_HOST_PASSWORD =
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
